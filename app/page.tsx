f1e8b723b40d5ec72b641632cbb7226135e8a2f2
Fernando Claverino <fernando.claverino@gmail.com>
2024-12-17 12:11:55 -0300

    use tools

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,22 @@
 import { useChat } from "ai/react";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    maxSteps: 5,
+  });
+
+  console.log(messages);
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((m) => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === "user" ? "User: " : "AI: "}
-          {m.content}
+          {m.toolInvocations ? (
+            <pre>{JSON.stringify(m.toolInvocations, null, 2)}</pre>
+          ) : (
+            <p>{m.content}</p>
+          )}
         </div>
       ))}
 
